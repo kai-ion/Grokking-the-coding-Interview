5709f69fee8f3fce290d90fea875ce6ad3ed0e36
kai-ion <43284426+kai-ion@users.noreply.github.com>
2021-12-21 16:29:53 -0500

    pattern 9

---
+++ 9. Pattern Two Heaps/1. Find the Median of a Number Stream (medium)/main (20).java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class MedianOfAStream {
+
+  public void insertNum(int num) {
+    // TODO: Write your code here
+  }
+
+  public double findMedian() {
+    // TODO: Write your code here
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    MedianOfAStream medianOfAStream = new MedianOfAStream();
+    medianOfAStream.insertNum(3);
+    medianOfAStream.insertNum(1);
+    System.out.println("The median is: " + medianOfAStream.findMedian());
+    medianOfAStream.insertNum(5);
+    System.out.println("The median is: " + medianOfAStream.findMedian());
+    medianOfAStream.insertNum(4);
+    System.out.println("The median is: " + medianOfAStream.findMedian());
+  }
+}
