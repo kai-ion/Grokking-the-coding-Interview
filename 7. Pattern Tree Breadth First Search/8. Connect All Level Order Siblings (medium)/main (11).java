a7877b456b2c334a698f63b81dfa17615423adcb
kai-ion <43284426+kai-ion@users.noreply.github.com>
2021-12-21 13:00:16 -0500

    pattern 7

---
+++ 7. Pattern Tree Breadth First Search/8. Connect All Level Order Siblings (medium)/main (11).java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode next;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = next = null;
+  }
+};
+
+class ConnectAllSiblings {
+  public static void connect(TreeNode root) {
+    // TODO: Write your code here
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(12);
+    root.left = new TreeNode(7);
+    root.right = new TreeNode(1);
+    root.left.left = new TreeNode(9);
+    root.right.left = new TreeNode(10);
+    root.right.right = new TreeNode(5);
+    ConnectAllSiblings.connect(root);
+
+    // level order traversal using 'next' pointer
+    TreeNode current = root;
+    System.out.println("Traversal using 'next' pointer: ");
+    while (current != null) {
+      System.out.print(current.val + " ");
+      current = current.next;
+    }
+  }
+}
