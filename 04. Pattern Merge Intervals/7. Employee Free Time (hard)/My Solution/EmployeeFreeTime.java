0aeb80807df109bfc03aa2c9544f57fcc0ef821c
kai <kaiion0509@gmail.com>
2022-01-04 12:43:44 -0500

    Update EmployeeFreeTime.java

--- 04. Pattern Merge Intervals/7. Employee Free Time (hard)/My Solution/EmployeeFreeTime.java
+++ 04. Pattern Merge Intervals/7. Employee Free Time (hard)/My Solution/EmployeeFreeTime.java
@@ -10,11 +10,61 @@ class Interval {
   }
 };
 
+class EmployeeSchedule {
+  Interval interval;
+  int employeeIndex;
+  int intervalIndex;
+
+  public EmployeeSchedule(Interval interval, int employeeIndex, int intervalIndex) {
+    this.interval = interval;
+    this.employeeIndex = employeeIndex;
+    this.intervalIndex = intervalIndex;
+  }
+}
+
 class EmployeeFreeTime {
 
   public static List<Interval> findEmployeeFreeTime(List<List<Interval>> schedule) {
     List<Interval> result = new ArrayList<>();
     // TODO: Write your code here
+
+    int len = shedule.size();
+    PriorityQueue<EmployeeSchedule> minHeap = new PriorityQueue<>
+      (len, (a,b) -> Integer.compare(a.interval.start, b.interval.start));
+
+    if (len == 0 || schedule == null) {
+      result.add(new Interval(0, 24));
+    }
+
+    for (int i = 0; i < len; i++) {
+      minHeap.offer(new EmployeeSchedule(schedule.get(i).get(0), i, 0));
+    }
+
+    Interval prevInterval = minHeap.peak().interval;
+    while (minHeap != null) {
+      EmployeeSchedule topQueue = minHeap.poll();
+
+      if (prevInterval.end < topQueue.interval.start) {
+        result.add(new Interval(prevInterval.end, topQueue.interval.start));
+        prevInterval = topQueue.interval;
+      }
+
+      else if (prevInterval.end < topQueue.interval.end) {
+
+        prevInterval = topQueue.interval.end;
+      }
+
+
+      List<Interval> employeeSchedule = schedule.get(topQueue.employeeIndex);
+
+      if (employeeSchedule.size() > topQueue.employeeIndex + 1) {
+        minHeap.offer(new EmployeeSchedule(
+            employeeSchedule.get(topQueue.intervalIndex + 1), topQueue.employeeIndex, topQueue.intervalIndex + 1));
+      }
+
+
+    }
+
     return result;
   }
 
