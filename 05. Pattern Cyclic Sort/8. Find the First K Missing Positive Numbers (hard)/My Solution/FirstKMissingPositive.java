7f97aaa2ce01ae3b23b847e4a9ccc61d7c96e8be
kai <kaiion0509@gmail.com>
2022-01-05 10:48:38 -0500

    file name change

--- 05. Pattern Cyclic Sort/8. Find the First K Missing Positive Numbers (hard)/My Solution/FirstKMissingPositive.java
+++ 05. Pattern Cyclic Sort/8. Find the First K Missing Positive Numbers (hard)/My Solution/FirstKMissingPositive.java
@@ -7,4 +7,15 @@ class FirstKMissingPositive {
     // TODO: Write your code here
     return missingNumbers;
   }
+
+  public static void main(String[] args) {
+    List<Integer> missingNumbers = FirstKMissingPositive.findNumbers(new int[] { 3, -1, 4, 5, 5 }, 3);
+    System.out.println("Missing numbers: " + missingNumbers);
+
+    missingNumbers = FirstKMissingPositive.findNumbers(new int[] { 2, 3, 4 }, 3);
+    System.out.println("Missing numbers: " + missingNumbers);
+
+    missingNumbers = FirstKMissingPositive.findNumbers(new int[] { -2, -3, 4 }, 2);
+    System.out.println("Missing numbers: " + missingNumbers);
+  }
 }
