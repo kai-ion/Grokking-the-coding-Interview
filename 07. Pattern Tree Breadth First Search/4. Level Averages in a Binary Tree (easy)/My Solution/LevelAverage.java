87f75e08f6f8e4637652bd5f94624865b1ed7474
kai <kaiion0509@gmail.com>
2022-01-08 00:14:21 -0500

    4. Level Averages in a Binary Tree (easy)

--- 07. Pattern Tree Breadth First Search/4. Level Averages in a Binary Tree (easy)/My Solution/LevelAverage.java
+++ 07. Pattern Tree Breadth First Search/4. Level Averages in a Binary Tree (easy)/My Solution/LevelAverage.java
@@ -14,6 +14,43 @@ class LevelAverage {
   public static List<Double> findLevelAverages(TreeNode root) {
     List<Double> result = new ArrayList<>();
     // TODO: Write your code here
+    if (root == null) {
+      return result;
+    }
+    
+    Queue<TreeNode> queue = new LinkedList<>();
+
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+
+      int levelSize = queue.size();
+      int sum = 0;
+      double average = 0;
+
+      for (int i = 0; i < levelSize; i++) {
+
+        TreeNode currentNode = queue.poll();
+
+        sum += currentNode.val;
+
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+
+
+      }
+
+      average = (double)sum/levelSize;
+      result.add(average);
+
+    }
+
+    
     return result;
   }
 
