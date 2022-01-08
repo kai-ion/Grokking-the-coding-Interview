7a69c58d3ccac1bf83303f2677953a3295e329e8
kai <kaiion0509@gmail.com>
2022-01-07 23:59:34 -0500

    3. Zigzag Traversal (medium)

--- 07. Pattern Tree Breadth First Search/3. Zigzag Traversal (medium)/My Solution/ZigzagTraversal.java
+++ 07. Pattern Tree Breadth First Search/3. Zigzag Traversal (medium)/My Solution/ZigzagTraversal.java
@@ -14,6 +14,47 @@ class ZigzagTraversal {
   public static List<List<Integer>> traverse(TreeNode root) {
     List<List<Integer>> result = new ArrayList<List<Integer>>();
     // TODO: Write your code here
+    if (root == null) {
+      return result;
+    }
+
+    Queue<TreeNode> queue= new LinkedList<>();
+
+    queue.offer(root);
+    Boolean leftRight = true;
+    
+    while (!queue.isEmpty()) {
+
+      int levelSize = queue.size();
+      
+      LinkedList<Integer> currentLevel = new LinkedList<>();
+
+
+      for (int i = 0; i < levelSize; i++) {
+
+        TreeNode currentNode = queue.poll();
+        
+        if (leftRight) {
+          currentLevel.add(currentNode.val);
+        }
+        else
+          currentLevel.add(0, currentNode.val);
+
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+
+
+      }
+
+      leftRight = !leftRight;
+      result.add(currentLevel);
+    }
+
     return result;
   }
 
