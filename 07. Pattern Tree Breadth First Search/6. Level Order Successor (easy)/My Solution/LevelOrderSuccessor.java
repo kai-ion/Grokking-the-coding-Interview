169a34cbe52f54effdf974fcad66f136e8dff9d4
kai <kaiion0509@gmail.com>
2022-01-08 16:59:46 -0500

    6. Level Order Successor (easy)

--- 07. Pattern Tree Breadth First Search/6. Level Order Successor (easy)/My Solution/LevelOrderSuccessor.java
+++ 07. Pattern Tree Breadth First Search/6. Level Order Successor (easy)/My Solution/LevelOrderSuccessor.java
@@ -13,6 +13,41 @@ class TreeNode {
 class LevelOrderSuccessor {
   public static TreeNode findSuccessor(TreeNode root, int key) {
     // TODO: Write your code here
+    if (root == null || key < 1) {
+      return null;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+
+    queue.offer(root);
+
+
+    while (!queue.isEmpty()) {
+
+      int levelSize = queue.size();
+
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+
+        
+
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+  
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+
+        if (currentNode.val == key) {
+          return queue.peek();
+        }
+      }
+
+      
+
+    }
+
     return null;    
   }
 
