7e599303eeb9b71778a997655ba1dd85cf8aaa42
kai <kaiion0509@gmail.com>
2022-01-07 22:19:52 -0500

    pattern Tree Breadth First Search

--- 07. Pattern Tree Breadth First Search/1. Binary Tree Level Order Traversal (easy)/My Solution/LevelOrderTraversal.java
+++ 07. Pattern Tree Breadth First Search/1. Binary Tree Level Order Traversal (easy)/My Solution/LevelOrderTraversal.java
@@ -14,6 +14,37 @@ class LevelOrderTraversal {
   public static List<List<Integer>> traverse(TreeNode root) {
     List<List<Integer>> result = new ArrayList<List<Integer>>();
     // TODO: Write your code here
+    if (root == null) {
+      return result;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+
+      ArrayList<Integer> currentLevel = new ArrayList<>(levelSize);
+
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+
+        currentLevel.add(currentNode.val);
+
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+
+      }
+
+      result.add(currentLevel);
+
+    }
+
     return result;
   }
 
