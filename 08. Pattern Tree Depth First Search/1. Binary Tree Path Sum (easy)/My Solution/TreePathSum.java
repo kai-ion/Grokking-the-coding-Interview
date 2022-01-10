d2649e43e4874985036ad7f05d720fa0c323c85e
kai <kaiion0509@gmail.com>
2022-01-09 22:30:17 -0500

    1. Binary Tree Path Sum (easy)

--- 08. Pattern Tree Depth First Search/1. Binary Tree Path Sum (easy)/My Solution/TreePathSum.java
+++ 08. Pattern Tree Depth First Search/1. Binary Tree Path Sum (easy)/My Solution/TreePathSum.java
@@ -12,7 +12,15 @@ class TreeNode {
 class TreePathSum {
   public static boolean hasPath(TreeNode root, int sum) {
     // TODO: Write your code here
-    return false;
+    if (root == null) {
+      return false;
+    }
+
+    if (root.val == sum && root.left == null && root.right == null) {
+      return true;
+    }
+
+    return hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val);
   }
 
   public static void main(String[] args) {
