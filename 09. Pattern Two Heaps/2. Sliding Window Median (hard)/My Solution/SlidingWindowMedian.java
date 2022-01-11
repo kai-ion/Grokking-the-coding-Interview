7642ec8e9699b8742e94a1f9b9e6f55b0c63e688
kai <kaiion0509@gmail.com>
2022-01-11 17:30:43 -0500

    2. Sliding Window Median (hard)

--- 09. Pattern Two Heaps/2. Sliding Window Median (hard)/My Solution/SlidingWindowMedian.java
+++ 09. Pattern Two Heaps/2. Sliding Window Median (hard)/My Solution/SlidingWindowMedian.java
@@ -1,10 +1,63 @@
 import java.util.*;
 
 class SlidingWindowMedian {
+
+  PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+  PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
   public double[] findSlidingWindowMedian(int[] nums, int k) {
     double[] result = new double[nums.length - k + 1];
     // TODO: Write your code here
+    int len = nums.length;
+
+    for (int i = 0; i < len; i++) {
+      int n = nums[i];
+
+      if (maxHeap.size() == 0 || n <= maxHeap.peek()) {
+        maxHeap.add(n);
+      }
+      else {
+        minHeap.add(n);
+      }
+
+      balance_heaps();
+      
+      if (i - k + 1 >= 0) {
+
+        if (maxHeap.size() == minHeap.size()) {
+          result[i - k + 1] = (maxHeap.peek() + minHeap.peek()) / 2.0; 
+        }
+        else {
+          result[i - k + 1] = maxHeap.peek();
+        }
+
+        int numOut = nums[i - k + 1];
+        if (maxHeap.peek() >= numOut) {
+          maxHeap.remove(numOut);
+        }
+        else {
+          minHeap.remove(numOut);
+        }
+        
+        balance_heaps();
+      }
+
+      
+
+    }
+
     return result;
+  }
+
+  private  void balance_heaps() {
+
+    if (maxHeap.size() > minHeap.size() + 1) {
+      minHeap.add(maxHeap.poll());
+    }
+    else if (minHeap.size() > maxHeap.size()) {
+      maxHeap.add(minHeap.poll());
+    }
+
   }
 
   public static void main(String[] args) {
