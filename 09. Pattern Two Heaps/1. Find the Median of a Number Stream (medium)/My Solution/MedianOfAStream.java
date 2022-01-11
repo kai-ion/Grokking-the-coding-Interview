710c49e40409ca175473ac8c07481e29e166d35a
kai <kaiion0509@gmail.com>
2022-01-11 15:01:47 -0500

    . Find the Median of a Number Stream (medium)

--- 09. Pattern Two Heaps/1. Find the Median of a Number Stream (medium)/My Solution/MedianOfAStream.java
+++ 09. Pattern Two Heaps/1. Find the Median of a Number Stream (medium)/My Solution/MedianOfAStream.java
@@ -2,13 +2,40 @@ import java.util.*;
 
 class MedianOfAStream {
 
+  Queue<Integer> maxHeap;
+  Queue<Integer> minHeap;
+
+  public MedianOfAStream() {
+    maxHeap = new PriorityQueue<>((a , b) -> b - a);
+    minHeap = new PriorityQueue<>((a , b) -> a - b);
+  }
+
   public void insertNum(int num) {
     // TODO: Write your code here
+    if (maxHeap.isEmpty() || maxHeap.peek() >= num) {
+      maxHeap.offer(num);
+    }
+    else {
+      minHeap.offer(num);
+    }
+
+    if (maxHeap.size() > minHeap.size() + 1) {
+      minHeap.offer(maxHeap.poll());
+    }
+
+    if (maxHeap.size() < minHeap.size()) {
+      maxHeap.offer(minHeap.poll());
+    }
+
   }
 
   public double findMedian() {
     // TODO: Write your code here
-    return -1;
+    if (maxHeap.size() == minHeap.size()) {
+      return (maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+
+    return maxHeap.peek();
   }
 
   public static void main(String[] args) {
