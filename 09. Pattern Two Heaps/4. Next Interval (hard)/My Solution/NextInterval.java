1487a5894fb068c5af229016b7758a866ecd9bde
kai <kaiion0509@gmail.com>
2022-01-12 16:40:53 -0500

    4. Next Interval (hard)

--- 09. Pattern Two Heaps/4. Next Interval (hard)/My Solution/NextInterval.java
+++ 09. Pattern Two Heaps/4. Next Interval (hard)/My Solution/NextInterval.java
@@ -14,6 +14,36 @@ class NextInterval {
   public static int[] findNextInterval(Interval[] intervals) {
     int[] result = new int[intervals.length];
     // TODO: Write your code here
+    int n = intervals.length;
+
+    PriorityQueue<Integer> maxStart = new PriorityQueue<>(n, (a, b) -> intervals[b].start - intervals[a].start);
+    PriorityQueue<Integer> maxEnd = new PriorityQueue<>(n, (a, b) -> intervals[b].end - intervals[a].end);
+
+    for (int i = 0; i < n; i++) {
+      maxEnd.offer(i);
+      maxStart.offer(i);
+    }
+
+    for (int i = 0; i < n; i++) {
+      int topEnd = maxEnd.poll();
+
+      result[topEnd] = -1;
+      
+      if (intervals[maxStart.peek()].start >= intervals[topEnd].end) {
+
+        int topStart = maxStart.poll();
+
+        while (maxStart.size() > 0 && intervals[maxStart.peek()].start >= intervals[topEnd].end) {
+          topStart = maxStart.poll();
+        }
+        result[topEnd] = topStart;
+
+        maxStart.offer(topStart);
+      }
+
+
+    }
+
     return result;
   }
 
