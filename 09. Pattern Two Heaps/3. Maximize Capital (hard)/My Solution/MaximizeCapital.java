9229bbc9dcd882f177bdf403d0f205e9cfa49bfa
kai <kaiion0509@gmail.com>
2022-01-11 19:35:24 -0500

    3. Maximize Capital (hard)

--- 09. Pattern Two Heaps/3. Maximize Capital (hard)/My Solution/MaximizeCapital.java
+++ 09. Pattern Two Heaps/3. Maximize Capital (hard)/My Solution/MaximizeCapital.java
@@ -1,9 +1,34 @@
 import java.util.*;
 
 class MaximizeCapital {
+
+  
+
   public static int findMaximumCapital(int[] capital, int[] profits, int numberOfProjects, int initialCapital) {
     // TODO: Write your code here
-    return -1;
+    int len = profits.length;
+    PriorityQueue<Integer> minCapital = new PriorityQueue<>(len, (a, b) -> capital[a] - capital[b]);
+    PriorityQueue<Integer> maxProfit = new PriorityQueue<>(len, (a, b) -> profits[b] - profits[a]);
+
+    for (int i = 0; i < len; i++) {
+      minCapital.offer(i);
+    }
+
+    for (int i = 0; i < numberOfProjects; i++) {
+
+      while (!minCapital.isEmpty() && initialCapital >= capital[minCapital.peek()]) {
+        maxProfit.offer(minCapital.poll());
+      }
+
+      if (maxProfit.isEmpty()) {
+        break;
+      }
+
+      initialCapital += profits[maxProfit.poll()];
+
+    }
+
+    return initialCapital;
   }
 
   public static void main(String[] args) {
