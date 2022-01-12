56dcc81876e62bb188a843fb26e18fd64d58e984
kai <kaiion0509@gmail.com>
2022-01-12 17:48:25 -0500

    1. Subsets (easy)

--- 10. Pattern Subsets/1. Subsets (easy)/My Solution/Subsets.java
+++ 10. Pattern Subsets/1. Subsets (easy)/My Solution/Subsets.java
@@ -5,6 +5,23 @@ class Subsets {
   public static List<List<Integer>> findSubsets(int[] nums) {
     List<List<Integer>> subsets = new ArrayList<>();
     // TODO: Write your code here
+
+    subsets.add(new ArrayList<>( ));
+
+    for (int num : nums) {
+
+      int n = subsets.size();
+
+      for (int i = 0; i < n; i++) {
+
+        List<Integer> set = new ArrayList<>(subsets.get(i));
+        set.add(num);
+
+        subsets.add(set);
+      }
+
+    }
+
     return subsets;
   }
 
