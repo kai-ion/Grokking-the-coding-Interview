9077a83f400a8cf1bcf60a89f2a18d548a18d09f
kai <kaiion0509@gmail.com>
2022-01-12 21:00:22 -0500

    2. Subsets With Duplicates (easy)

--- 10. Pattern Subsets/2. Subsets With Duplicates (easy)/My Solution/SubsetWithDuplicates.java
+++ 10. Pattern Subsets/2. Subsets With Duplicates (easy)/My Solution/SubsetWithDuplicates.java
@@ -5,6 +5,32 @@ class SubsetWithDuplicates {
   public static List<List<Integer>> findSubsets(int[] nums) {
     List<List<Integer>> subsets = new ArrayList<>();
     // TODO: Write your code here   
+    int len = nums.length;
+
+    subsets.add(new ArrayList<>());
+
+    int start, end = 0;
+    for (int i = 0; i < len; i++) {
+
+      
+      start = 0;
+
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        start = end + 1;
+      }
+
+      end = subsets.size();
+
+      for (int j = start; j < end; j++) {
+        ArrayList<Integer> set = new ArrayList<>(subsets.get(j));
+        set.add(nums[i]);
+
+        subsets.add(set);
+      }
+
+    }
+
+
     return subsets;
   }
 
