f0ab5b6dea187356ebc8b349e5a2e613bb3bf900
kai <kaiion0509@gmail.com>
2022-01-16 16:46:52 -0500

    5. Balanced Parentheses (hard)

--- 10. Pattern Subsets/5. Balanced Parentheses (hard)/My Solution/GenerateParentheses.java
+++ 10. Pattern Subsets/5. Balanced Parentheses (hard)/My Solution/GenerateParentheses.java
@@ -1,10 +1,45 @@
 import java.util.*;
 
+class paranthesesString {
+  String str;
+  int openCount;
+  int closeCount;
+
+  public paranthesesString(String s, int openCount, int closeCount) {
+    str = s;
+    this.openCount = openCount;
+    this.closeCount = closeCount;
+  }
+}
+
 class GenerateParentheses {
+
+
 
   public static List<String> generateValidParentheses(int num) {
     List<String> result = new ArrayList<String>();
     // TODO: Write your code here
+    Queue<paranthesesString> queue = new LinkedList<>();
+    queue.add(new paranthesesString("", 0, 0));
+
+    while (!queue.isEmpty()) {
+      paranthesesString ps = queue.poll();
+
+      if (ps.openCount == num && ps.closeCount == num) {
+        result.add(ps.str);
+      } else {
+        if (ps.openCount < num) {
+          queue.add(new paranthesesString(ps.str + "(", ps.openCount + 1, ps.closeCount));
+        }
+  
+        if (ps.openCount > ps.closeCount) {
+          queue.add(new paranthesesString(ps.str + ")", ps.openCount, ps.closeCount + 1));
+        }
+      }
+
+    }
+
+
     return result;
   }
 
