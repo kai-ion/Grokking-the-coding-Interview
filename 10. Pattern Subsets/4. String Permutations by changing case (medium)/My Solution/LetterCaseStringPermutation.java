5bd3efd782c20562c9cb4879d1c67ee6c7413b82
kai <kaiion0509@gmail.com>
2022-01-16 12:41:37 -0500

    4. String Permutations by changing case (medium)

--- 10. Pattern Subsets/4. String Permutations by changing case (medium)/My Solution/LetterCaseStringPermutation.java
+++ 10. Pattern Subsets/4. String Permutations by changing case (medium)/My Solution/LetterCaseStringPermutation.java
@@ -5,6 +5,38 @@ class LetterCaseStringPermutation {
   public static List<String> findLetterCaseStringPermutations(String str) {
     List<String> permutations = new ArrayList<>();
     // TODO: Write your code here
+    if (str == null) {
+      return permutations;
+    }
+
+    int len = str.length();
+    permutations.add(str);
+
+    for (int i = 0; i < len; i++) {
+
+      if (Character.isLetter(str.charAt(i))) {
+        
+        int permLen = permutations.size();
+
+        for (int j = 0; j < permLen; j++) {
+
+          char[] chs = permutations.get(i).toCharArray();
+
+          if (Character.isUpperCase(chs[i])) {
+            chs[i] = Character.toLowerCase(chs[i]);
+          }
+          else if (Character.isLowerCase(chs[i])) {
+            chs[i] = Character.toUpperCase(chs[i]);
+          }
+
+          permutations.add(String.valueOf(chs));
+
+        }
+
+      }
+
+    }
+
     return permutations;
   }
 
