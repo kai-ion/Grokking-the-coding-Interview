a28a10f8e1d651ef56e0dcfc3b84bb69913dffdc
kai-ion <43284426+kai-ion@users.noreply.github.com>
2022-01-18 15:15:11 -0500

    6. Unique Generalized Abbreviations (hard)

--- 10. Pattern Subsets/7. Evaluate Expression (hard)/main (6).java
+++ 10. Pattern Subsets/7. Evaluate Expression (hard)/main (6).java
@@ -4,6 +4,41 @@ class EvaluateExpression {
   public static List<Integer> diffWaysToEvaluateExpression(String input) {
     List<Integer> result = new ArrayList<>();
     // TODO: Write your code here
+
+    if (!input.contains("+") && !input.contains("*") && !input.contains("-")) {
+      result.add(Integer.parseInt(input));
+    }else {
+      int len = input.length();
+
+      for (int i = 0; i < len; i++) {
+
+        char ch = input.charAt(i);
+        if (!Character.isDigit(ch)) {
+          List<Integer> left = diffWaysToEvaluateExpression(input.substring(0, i));
+          List<Integer> right = diffWaysToEvaluateExpression(input.substring(i + 1));
+
+          for (int part1 : left) {
+
+            for (int part2 : right) {
+              if (ch == '+') {
+                result.add(part1 + part2);
+              }
+              else if (ch == '-') {
+                result.add(part1 - part2);
+              }
+              else if (ch == '*') {
+                result.add(part1 * part2);
+              }
+            }
+          }
+        }
+     
+
+      }
+
+    }
+
+
     return result;
   }
 
