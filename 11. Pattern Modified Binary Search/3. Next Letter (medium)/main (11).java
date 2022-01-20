f8f5fa8fd5d2b3f74dd187dddb5530ebdefdf366
kai-ion <43284426+kai-ion@users.noreply.github.com>
2022-01-20 14:39:57 -0500

    6. Unique Generalized Abbreviations (hard)

--- 11. Pattern Modified Binary Search/3. Next Letter (medium)/main (11).java
+++ 11. Pattern Modified Binary Search/3. Next Letter (medium)/main (11).java
@@ -2,7 +2,22 @@ class NextLetter {
 
   public static char searchNextLetter(char[] letters, char key) {
     // TODO: Write your code here
-    return letters[0];
+    int len = letters.length;
+
+    int start = 0;
+    int end = len - 1;
+
+    while ( start <= end) {
+      int mid = start + (end - start)/2;
+
+      if (key < letters[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+
+    return letters[start % len];
   }
 
   public static void main(String[] args) {
