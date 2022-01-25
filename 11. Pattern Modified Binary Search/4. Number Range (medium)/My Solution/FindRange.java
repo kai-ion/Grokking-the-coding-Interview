8b11830ea3b9ce576458cf97a6dec72354a27ed6
kai-ion <43284426+kai-ion@users.noreply.github.com>
2022-01-25 12:40:08 -0500

    solution

--- 11. Pattern Modified Binary Search/4. Number Range (medium)/My Solution/FindRange.java
+++ 11. Pattern Modified Binary Search/4. Number Range (medium)/My Solution/FindRange.java
@@ -3,8 +3,39 @@ class FindRange {
   public static int[] findRange(int[] arr, int key) {
     int[] result = new int[] { -1, -1 };
     // TODO: Write your code here
+    result[0] = search(arr, key, false);
+    if (result[0] != -1) {
+      result[1] = search(arr, key, true);
+    }
     return result;
   }
+
+  private static int search (int[] arr, int key, boolean found) {
+    int index = -1;
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start <= end) {
+      int mid = start + (end - start)/2;
+      
+      if (arr[mid] > key) {
+        end = mid - 1;
+      } else if (arr[mid] < key) {
+        start = mid + 1;
+      } else {
+        index = mid;
+        if (found) {
+          start = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      }
+
+    }
+
+    return index;
+  }
+
 
   public static void main(String[] args) {
     int[] result = FindRange.findRange(new int[] { 4, 6, 6, 6, 9 }, 6);
