c91cd2912e1faa93b32b7b6188f2195763d0aa9d
kai-ion <43284426+kai-ion@users.noreply.github.com>
2022-01-27 14:57:06 -0500

    incomplete solution

--- 11. Pattern Modified Binary Search/8. Search Bitonic Array (medium)/My Solution/SearchBitonicArray.java
+++ 11. Pattern Modified Binary Search/8. Search Bitonic Array (medium)/My Solution/SearchBitonicArray.java
@@ -2,7 +2,49 @@ class SearchBitonicArray {
 
   public static int search(int[] arr, int key) {
     // TODO: Write your code here
+    int max_index = find_Max(arr);
     return -1;
+  }
+
+  public static int find_Max(int[] arr) {
+
+    int start = 0;
+    int end = arr.lenth - 1;
+
+    while (start <- end) {
+      int mid = start + (end - start)/2;
+
+      if (arr[mid] < arr[mid + 1]) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
+
+    return start;
+  }
+
+  public static int bindary_Search (int[] arr, int key, int start, int end) {
+
+    while (start <= end) {
+
+      int mid = start + (end - start)/2
+
+      if (key == arr[mid]) {
+        return mid;
+      }
+
+      if (arr[start] < arr[end]) {
+
+        if (arr[mid] < key) {
+          start = mid + 1;
+        } else {
+          end = mid;
+        }
+      }
+    }
+
+    return start;
   }
 
   public static void main(String[] args) {
